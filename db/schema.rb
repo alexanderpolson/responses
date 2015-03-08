01050256041c7b068ead89c9a29e1af11704fef1
Alex Polson <apolson@amazon.com>
2015-03-07 22:08:30 -0800

    Use PLSQL instead of SQLite.

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20150308001705) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "categories", ["name"], name: "index_categories_on_name", unique: true
+  add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
 
   create_table "responses", force: :cascade do |t|
     t.string   "content"
@@ -28,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150308001705) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "responses", ["category_id"], name: "index_responses_on_category_id"
+  add_index "responses", ["category_id"], name: "index_responses_on_category_id", using: :btree
 
+  add_foreign_key "responses", "categories"
 end
