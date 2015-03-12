0f44f8dd85db968ef0f66ea4507ae9aa24758430
Alex Polson <apolson@amazon.com>
2015-03-11 20:27:01 -0700

    Log request parameters.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :log_request
+
   def render_404
-    
+
+  end
+
+  private
+  def log_request
+    Rails.logger.info("Request Parameters: #{request.inspect}")
   end
 end
